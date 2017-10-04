bd3402530986c277f82474b3a3fc755c4dc8ffff
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-04 13:28:52 +0100

    Chapter 4, Variables & assignment

---
+++ variables.rb
@@ -0,0 +1,4 @@
+name = "Marie Alice Astrid Kerkstoel"
+puts "Hello, my name is #{name}. I would like to sign in."
+puts "You can assign an object to a variable and we can reassign."
+puts "Short piece of theory. if we assign var1 to the number 8 and we assign var2 to var1. If we then change var1 to something else, let's say 9. var1 then = 9 and var2 = 8. var2 didn't change with var1"
