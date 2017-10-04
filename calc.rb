28c4c25c46bd9ab7296bfd27dbd7a6caf5780bb2
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-04 12:21:39 +0100

    Chapter 2, numbers

---
+++ calc.rb
@@ -0,0 +1,7 @@
+puts 5 *(12-8) + -15
+hours_in_year = 365 * 24
+minutes_in_decade = 10 * 365 * 24 * 60
+age_in_seconds = 23 * 365 * 24 * 60 * 60
+age_author = 1160000000 / 60 / 60 / 24 / 365
+
+puts age_author
