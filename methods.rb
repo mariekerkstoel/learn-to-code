d53bb56c2f98e1b2d99c203c1b9d2de711605023
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-04 15:16:02 +0100

    Chapter 6, The math object

---
+++ methods.rb
@@ -0,0 +1,16 @@
+puts "What do you want?"
+wants = gets.chomp
+wants_c = wants.upcase
+puts " WHAT DO YOU MEAN YOU WANT #{wants_c}?!"
+
+srand 1445
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+
+srand 1445
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
