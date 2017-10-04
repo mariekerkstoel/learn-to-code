8e89d93f471846374b4780c68cdc3d48b291e8c7
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-04 12:32:41 +0100

    Chapter 3 , Letters

---
+++ strings.rb
@@ -0,0 +1,8 @@
+puts "Hello, world!"
+puts"goodbye"
+puts "I like" + "people"
+puts "I made a mistake" +" should have put a space between like and people"
+puts "blink" * 2
+puts "Important to remember is that you can't really add a number and a string"
+puts "Never try to put 2 * blink"
+puts "let me try and \'escape."
