d8aada8d125930bd2d6af0822ad0797721f29c68
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-07 20:40:27 +0100

    Ex1 chapter 5

---
+++ chapter5/ex1.rb
@@ -0,0 +1,8 @@
+puts "Can you give me your first name please"
+first_name = gets.chomp
+puts "Any middle names?"
+middle_name = gets.chomp
+puts "Last name"
+last_name = gets.chomp
+
+puts "Your full name = #{first_name} #{middle_name} #{last_name}"
