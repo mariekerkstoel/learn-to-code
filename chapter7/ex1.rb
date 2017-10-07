05fe087d51b73d7cea9f996b6064342d039a7853
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-07 21:18:02 +0100

    Ex1 , chapter7

---
+++ chapter7/ex1.rb
@@ -0,0 +1,6 @@
+bottles_of_beer = 99
+while bottles_of_beer > 0
+puts "#{bottles_of_beer} bottles of beer on the wall #{bottles_of_beer} bottles of beer!"
+bottles_of_beer -= 1
+puts "take one down, pass it around #{bottles_of_beer} bottles on the wall! "
+end
